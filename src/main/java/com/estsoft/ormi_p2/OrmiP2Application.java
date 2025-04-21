f97bf100a6a967d64311726b6c4c1c5b692b55fd
tetsuya <cinemamyg@gmail.com>
2025-04-21 01:23:51 -0400

    Initial commit

---
+++ src/main/java/com/estsoft/ormi_p2/OrmiP2Application.java
@@ -0,0 +1,13 @@
+package com.estsoft.ormi_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmiP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmiP2Application.class, args);
+	}
+
+}
