74ca96c53a50ca06be58b16f0a2aa1369a0cf5d0
tetsuya <cinemamyg@gmail.com>
2025-04-21 04:54:11 -0400

    fixed build error

--- src/main/java/com/estsoft/ormi_p2/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.estsoft.ormi_p2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository {
-}
