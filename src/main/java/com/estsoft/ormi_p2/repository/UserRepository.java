b8cc91b39f2d6242bb07d698ff9bb5b638e85501
jiye21 <jmhy0921@naver.com>
2025-04-21 15:36:26 +0900

    add repository

---
+++ src/main/java/com/estsoft/ormi_p2/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.estsoft.ormi_p2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository {
+}
